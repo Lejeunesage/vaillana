e9d6156509901060c76c93466a193bdf0c74500d
ObafemiEuloge <eagnoun@gmail.com>
2024-03-22 21:18:39 +0100

    Mise en place et configuration du frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        JakartaSans: ['JakartaSans', 'sans-serif'],
+        Ballsye: ['Ballsye', 'sans-serif'],
+        Komikula: ['Komikula', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
