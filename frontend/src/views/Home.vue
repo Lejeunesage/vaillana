1d1df01e3a1fd879157e9ab7381cd0fbc10863c8
awarrisw <awarrisw@outlook.fr>
2024-03-23 12:30:42 +0100

    Mise en forme de la première partie du home okay

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,7 +4,7 @@
 
 <template>
   <main class="lg:max-w-5xl xl:max-w-6xl mx-auto">
-    <section class="bg-white py-6 text-center">
+    <section class="bg-white py-6 text-center flex flex-col gap-10 justify-center">
 
       <!-- Section du texte -->
       <section class="flex justify-center flex-col">
@@ -14,15 +14,17 @@
         <div class="flex flex-col gap-10 justify-center">
           <p class="uppercase font-Komikula font-bold">Votre <span class="text-[#0398C7]">site web</span> en un clic,
             tout-en-un. simple, efficace, abordable.</p>
-          <RouterLink to="/chatbot">
-            <span
-              class="bg-gradient w-fit text-white font-Acumin text-center p-1 px-2 rounded-full uppercase font-bold bg-white">Commencer</span>
-          </RouterLink>
+
         </div>
       </section>
       <!-- Section d'ajout d'image -->
-      <!-- <img src="../" /> -->
-
+      <img src="../../public/assets/img/ressources/pinchot.png" width="70%" class="mx-auto" />
+      <div>
+        <RouterLink to="/chatbot">
+          <span
+            class="bg-gradient w-fit text-white font-Acumin text-center p-1 px-3 rounded-full uppercase font-bold bg-white">Commencer</span>
+        </RouterLink>
+      </div>
     </section>
     <hr class="h-1 bg-gradient" />
     <section class="bg-blue-500 my-12">
