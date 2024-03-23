c39d16c130d77d608559e12ad87c090294f3334b
awarrisw <awarrisw@outlook.fr>
2024-03-23 10:57:38 +0100

    Ajout des polices nécessaires

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,22 +1,23 @@
 <template>
-    <header class="bg-blue-300 flex justify-between items-center p-4">
-        <div class>
-            <!-- Ajout du Nom du site -->
-            <h1>VAILLANA</h1>
-        </div>
-
-        <!-- Menu d'Hamburger -->
-        <div class="wrapper">
-            <svg v-if="openMenu" @click="toggleMenu()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list showAnimate"
-                viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-            </svg>
-            <svg v-if="!openMenu" @click="toggleMenu()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x-lg showAnimate"
-                viewBox="0 0 16 16">
-                <path
-                    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-            </svg>
+    <header class="p-4 text-white">
+        <div class="flex justify-between items-center">
+            <div class>
+                <!-- Ajout du Nom du site -->
+                <h1 class="font-Ballsye">VAILLANA</h1>
+            </div>
+            <!-- Menu d'Hamburger -->
+            <div class="wrapper">
+                <svg v-if="openMenu" @click="toggleMenu()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list showAnimate"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                </svg>
+                <svg v-if="!openMenu" @click="toggleMenu()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x-lg showAnimate"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                </svg>
+            </div>
         </div>
     </header>
     <!-- <div class="wrapper">
@@ -61,4 +62,21 @@ import { ref } from 'vue';
 
 <style scoped>
 /*** Animation d'apparition du menu */
+
+.showAnimate {
+    animation: showAnimation 1.5s ease-in-out;
+}
+
+@keyframes showAnimation {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+
+header {
+    background: linear-gradient(to right,#0398C7  0%,#9747FF  100%);
+}
 </style>
