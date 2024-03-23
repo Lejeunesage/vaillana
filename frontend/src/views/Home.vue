a9681a3016c8ea0e2af369fa3df5288977995382
awarrisw <awarrisw@outlook.fr>
2024-03-23 12:37:03 +0100

    Mise en forme de la première partie du home en cours

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,26 +4,31 @@
 
 <template>
   <main class="lg:max-w-5xl xl:max-w-6xl mx-auto">
-    <section class="bg-white py-6 text-center flex flex-col gap-10 justify-center">
-
+    <section class="bg-white py-6 text-center md:flex md:pl-10">
       <!-- Section du texte -->
       <section class="flex justify-center flex-col">
-        <h1 class="text-5xl text-gradient font-Ballsye leading-tight">
+        <h1 class="text-5xl md:text-6xl text-gradient font-Ballsye leading-tight">
           VAILLANA
         </h1>
         <div class="flex flex-col gap-10 justify-center">
-          <p class="uppercase font-Komikula font-bold">Votre <span class="text-[#0398C7]">site web</span> en un clic,
+          <p class="uppercase font-Komikula font-bold md:text-2xl">Votre <span class="text-[#0398C7]">site web</span> en
+            un clic,
             tout-en-un. simple, efficace, abordable.</p>
-
+          <RouterLink class="hidden md:block" to="/chatbot">
+            <span
+              class="bg-gradient w-fit text-white font-Acumin text-center p-2 px-4 rounded-full uppercase font-bold bg-white">Commencer</span>
+          </RouterLink>
         </div>
       </section>
-      <!-- Section d'ajout d'image -->
-      <img src="../../public/assets/img/ressources/pinchot.png" width="70%" class="mx-auto" />
       <div>
-        <RouterLink to="/chatbot">
-          <span
-            class="bg-gradient w-fit text-white font-Acumin text-center p-1 px-3 rounded-full uppercase font-bold bg-white">Commencer</span>
-        </RouterLink>
+        <!-- Section d'ajout d'image -->
+        <img src="../../public/assets/img/ressources/pinchot.png" width="70%" class="mx-auto my-8 md:m-0" />
+        <div class="md:hidden">
+          <RouterLink to="/chatbot">
+            <span
+              class="bg-gradient w-fit text-white font-Acumin text-center p-1 px-3 rounded-full uppercase font-bold bg-white">Commencer</span>
+          </RouterLink>
+        </div>
       </div>
     </section>
     <hr class="h-1 bg-gradient" />
