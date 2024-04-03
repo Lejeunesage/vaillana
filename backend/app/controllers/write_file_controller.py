a444f30d0a317e3ac9405e7376a5133d6d0fc061
Kola771 <koladeaboudou@gmail.com>
2024-04-03 13:12:45 +0200

    commit

--- backend/app/controllers/write_file_controller.py
+++ backend/app/controllers/write_file_controller.py
@@ -103,8 +103,7 @@ def downloadImage(destination, chemin, occurence, urls):
                 lines_find = [i for i, line in enumerate(lines) if occurence in line]
 
             if (len(all_occurences) > 0) :
-                if(len(all_occurences) == len(images)):
-                    [lines.__setitem__(x, re.sub(all_occurences[i], images[i], lines[x], flags=re.MULTILINE) + '\n') for i, x in enumerate(lines_find) if 0 <= x < len(lines)]
+                [lines.__setitem__(x, re.sub(all_occurences[i], images[i], lines[x], flags=re.MULTILINE) + '\n') for i, x in enumerate(lines_find) if 0 <= x < len(lines)]
                 
                 # Écrire la liste modifiée dans le fichier
                 with open(chemin, 'w', encoding='utf-8') as file:
@@ -151,12 +150,12 @@ def writeFileCss(line, search, valeur):
         return {"error": "Ce fichier n'existe pas !"}
 
 # Fonction pour écrire dans tous les fichiers se trouvant dans le template
-@router.get("/write-file")
-def writeFile(nameDossier='kola_dev'):
+@router.post("/write-file")
+def writeFile(nameDossier, nameTemplate, primary, secondary, tertiairy):
     # Nouvelles couleurs du template
-    colors_template = ["#ffffff", "#c33", "#336699"]
+    colors_template = [primary, secondary, tertiairy]
     # Dossier contenant tout le code du site
-    source = "./exemples/Template1"
+    source = "./exemples/" + nameTemplate
 
     nameSite = nameDossier.replace(" ", "_")
     # Chemin de destination pour la copie du dossier
@@ -181,14 +180,62 @@ def writeFile(nameDossier='kola_dev'):
                     variable = 'my_article_variable_yx_2024_hf_cp'
                     # Liste contenant les articles
                     articles = [
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ex quos voluptatem blanditiis eveniet eum itaque. Harum magnam explicabo officia vitae quam aut minus numquam veniam facilis vero, sit, architecto incidunt, ad amet qui deserunt. Modi officiis, facilis recusandae eaque aperiam suscipit, tenetur cupiditate ipsa atque maiores dignissimos obcaecati quos eligendi perspiciatis. Quo blanditiis dicta quasi porro dolore dolorem nihil incidunt, quisquam eveniet labore, amet placeat earum modi. Harum nam eligendi, numquam facilis quibusdam quod sed voluptatem dolor.",
-                        "Porro ipsa esse totam sunt error officiis pariatur impedit iste in. Obcaecati sint qui tenetur at modi veniam quisquam, sequi laudantium, officia harum atque! Velit a nam, praesentium sapiente cumque ab accusamus dolorem modi consequuntur, quam perferendis sunt incidunt ut! Dolorum minus iure unde! Omnis fuga iure quod, earum totam reprehenderit possimus saepe est tenetur qui error quasi veritatis eos repudiandae odio facere eligendi sequi maxime labore obcaecati quia esse velit quam. Qui dolorem quia voluptatibus cupiditate debitis nesciunt corrupti.",
-                        "Libero praesentium explicabo autem beatae vero, magnam eos expedita numquam voluptates pariatur dolore neque sequi quos, porro iusto, rerum non iste quasi nemo similique tenetur dolorum a in quae? Saepe, amet mollitia? Amet, expedita cumque enim in quos veniam iure voluptas doloremque ipsum molestiae earum quam vitae ratione ipsam ipsa libero sapiente fuga nihil quo dicta molestias? Ipsum eveniet voluptates sint ex ut dolores, fugit eos, quod quisquam at saepe quibusdam magnam cumque consequuntur molestias possimus laudantium sapiente placeat. Sit, recusandae sunt nisi, omnis velit quidem necessitatibus voluptate non itaque laboriosam, magnam quasi fuga! Quidem, dolorum tempore repudiandae animi incidunt vero obcaecati fugiat fuga, aspernatur aperiam illum, voluptas accusamus id suscipit cupiditate modi nobis quisquam atque ad sit? Animi id consequatur neque, rerum blanditiis, itaque aspernatur iure minima accusamus praesentium officiis quas nisi harum voluptates molestiae tenetur ipsa possimus repellendus rem, hic maxime voluptatum illo. Corporis inventore sint, quas vel, laborum distinctio porro rerum placeat repellendus nostrum fugiat odio. Deserunt animi rem perspiciatis autem ipsam ut? Animi architecto molestias ab dolore, officia aut magni repudiandae minus, eum consequatur inventore aliquam aperiam sapiente hic ducimus eius reiciendis quis quam consectetur mollitia maiores fugit porro facilis? Ad consequatur quis beatae eum enim fugit, possimus nam ipsa dicta accusamus perferendis rerum iure, itaque architecto tempore dolorem recusandae totam exercitationem! Dolorum, reiciendis corrupti! Saepe veritatis ut deleniti laudantium eos voluptatum modi placeat deserunt natus laboriosam, non provident, beatae consectetur dignissimos officia dolores soluta. Exercitationem, dicta laudantium corporis numquam quasi id quibusdam veritatis architecto nisi nesciunt recusandae! Dignissimos, commodi. Nulla, veniam porro. Culpa itaque nesciunt dolorem hic illum repellat amet sapiente vitae doloremque corporis ipsam molestias, sequi nulla dolor quisquam suscipit voluptatum eos nihil natus maiores! Odio alias maiores atque, a, hic eligendi doloremque cupiditate adipisci autem earum velit fugit minima commodi, totam laboriosam tempora esse praesentium eum!",
-                        "Blanditiis, iusto quisquam ipsa dolorum quos aspernatur commodi autem voluptatibus doloremque ea! Laboriosam vero dolorem exercitationem totam neque commodi consectetur tempora ex aut libero, alias facilis minus eveniet laborum, enim nihil temporibus voluptatem assumenda, quo dicta architecto magnam inventore obcaecati? Dolorum quaerat quos ad, ipsam asperiores beatae maiores sed? Earum reprehenderit aliquid obcaecati enim repellat? Accusamus sapiente laboriosam maxime veritatis nam soluta alias rem, harum in magnam iure minima nostrum vel ipsa. Est explicabo quo et nobis non accusamus nemo nesciunt amet, sit natus quam dolores! Voluptatibus quisquam porro cumque facilis pariatur earum harum quam velit aliquam eum illo recusandae accusamus alias minima repellendus nihil, tempora, maiores aut deleniti quas? Amet, tempora pariatur consequatur aliquid eum veniam, qui expedita dignissimos corrupti, saepe fugiat dicta ducimus quisquam aspernatur quaerat ipsa voluptate mollitia veritatis est rerum aut excepturi minima officia. Neque, cumque soluta! Sapiente unde debitis fugiat, numquam tenetur in voluptatum quia voluptas. Earum a, officiis suscipit praesentium veritatis eum iusto natus est, id eius illo, voluptatem recusandae ipsa. Possimus unde quia non omnis nihil! Ex culpa eum hic eaque reprehenderit iste ipsam. Atque facere sint repudiandae tempora maiores quisquam nobis dolor eaque explicabo laborum, quo perspiciatis, velit numquam ex consequuntur exercitationem dignissimos harum odio ad omnis officiis? Porro cumque voluptas ullam consequuntur necessitatibus soluta non, consectetur corporis? Quisquam accusantium praesentium quis totam laborum est earum esse, minus enim minima consectetur repellendus facilis tenetur commodi corrupti ducimus dolores ea quas iure nulla nihil saepe perferendis sunt?"
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ex quos voluptatem blanditiis eveniet eum itaque. Harum magnam explicabo officia vitae quam aut minus numquam veniam.",
+                        "Porro ipsa esse totam sunt error officiis pariatur impedit iste in. Obcaecati sint qui tenetur at modi veniam quisquam, sequi laudantium, officia harum atque! Velit a nam, praesentium sapiente cumque.",
+                        "Blanditiis, iusto quisquam ipsa dolorum quos aspernatur commodi autem voluptatibus doloremque ea! Laboriosam vero dolorem exercitationem totam neque commodi consectetur tempora ex aut libero."
                     ]
 
                     # Je rappelle ma fonction de réecriture des articles du site
                     writeArticle = writeArticleOfSite(chemin, variable, articles)
+
+                    # Je rappelle ma fonction de réecriture des titres des articles
+                    variable1 = 'title_yx_2024_hf_cp'
+                    # Liste contenant les titres des articles
+                    titles_articles = [
+                        "Titre 1",
+                        "Titre 2",
+                        "Titre 3"
+                    ]
+                    writeTitleArticle = writeArticleOfSite(chemin, variable1, titles_articles)
+
+                    # Je rappelle ma fonction de réecriture des titres des services
+                    variable2 = 'title_service_yx_2024_hf_cp'
+                    # Liste contenant les titres des services
+                    titles_services = [
+                        "Service 1",
+                        "Service 2",
+                        "Service 3"
+                    ]
+                    writeTitleService = writeArticleOfSite(chemin, variable2, titles_services)
+
+                    # Je rappelle ma fonction de réecriture des articles des services
+                    variable3 = 'article_service_yx_2024_hf_cp'
+                    # Liste contenant les articles des services
+                    articles_services = [
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ex quos voluptatem blanditiis eveniet eum itaque. Harum magnam explicabo officia vitae quam aut minus numquam veniam.",
+                        "Porro ipsa esse totam sunt error officiis pariatur impedit iste in. Obcaecati sint qui tenetur at modi veniam quisquam, sequi laudantium, officia harum atque! Velit a nam, praesentium sapiente cumque.",
+                        "Blanditiis, iusto quisquam ipsa dolorum quos aspernatur commodi autem voluptatibus doloremque ea! Laboriosam vero dolorem exercitationem totam neque commodi consectetur tempora."
+                    ]
+                    writeArticleService = writeArticleOfSite(chemin, variable3, articles_services)
+
+                    # Liste d'images
+                    urls_images = ["https://cdn.futura-sciences.com/cdn-cgi/image/width=1920,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg", "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg", "https://img.freepik.com/photos-gratuite/beaute-abstraite-automne-dans-motif-veines-feuilles-multicolores-genere-par-ia_188544-9871.jpg", "https://etudestech.com/wp-content/uploads/2023/05/midjourney-1536x1024.jpeg"]
+
+                    # Je rappelle ma fonction de réecriture de l'à propos
+                    variable4 = 'contenu_about_yx_2024_hf_cp'
+                    body_about = [
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        "Porro ipsa esse totam sunt error officiis pariatur impedit iste in. Obcaecati sint qui tenetur at modi veniam quisquam, sequi laudantium, officia harum atque! Velit a nam, praesentium sapiente cumque. Blanditiis, iusto quisquam ipsa dolorum quos aspernatur commodi autem voluptatibus doloremque ea! Laboriosam vero dolorem exercitationem totam neque commodi consectetur tempora. Blanditiis, iusto quisquam ipsa dolorum quos aspernatur commodi autem voluptatibus doloremque ea! Laboriosam vero dolorem exercitationem totam neque commodi consectetur tempora."
+                    ]
+                    writeBodyAbout = writeArticleOfSite(chemin, variable4, body_about)
+
+                    # Je rappelle ma fonction de réecriture de la présentation
+                    variable5 = 'presentation_yx_2024_hf_cp'
+                    presations_lists = [
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        "Porro ipsa esse totam sunt error officiis pariatur impedit iste in. Obcaecati sint qui tenetur at modi veniam quisquam, sequi laudantium, officia harum atque! Velit a nam, praesentium sapiente cumque."
+                    ]
+                    writePresentation = writeArticleOfSite(chemin, variable5, presations_lists)
 
                     # Liste d'images
                     urls_images = ["https://cdn.futura-sciences.com/cdn-cgi/image/width=1920,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg", "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg", "https://img.freepik.com/photos-gratuite/beaute-abstraite-automne-dans-motif-veines-feuilles-multicolores-genere-par-ia_188544-9871.jpg", "https://etudestech.com/wp-content/uploads/2023/05/midjourney-1536x1024.jpeg"]
@@ -197,27 +244,41 @@ def writeFile(nameDossier='kola_dev'):
 
                     # Je rappelle ma fonction de téléchargement des images et de réecriture
                     downloads = downloadImage(destination, chemin, word_found, urls_images)
+
+                    # Liste d'images
+                    urls_images_about = ["https://cdn.futura-sciences.com/cdn-cgi/image/width=1920,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg"]
+
+                    word_found_about = "my_file_about_jpg_yx_2024_hf_cp"
+
+                    # Je rappelle ma fonction de téléchargement des images et de réecriture
+                    downloads = downloadImage(destination, chemin, word_found_about, urls_images_about)
+
+                    # Liste d'images
+                    urls_images_presentation = ["https://img.freepik.com/photos-gratuite/beaute-abstraite-automne-dans-motif-veines-feuilles-multicolores-genere-par-ia_188544-9871.jpg", "https://etudestech.com/wp-content/uploads/2023/05/midjourney-1536x1024.jpeg"]
+
+                    word_found_presentation = "banner_jpg_yx_2024_hf_cp"
+
+                    # Je rappelle ma fonction de téléchargement des images et de réecriture
+                    downloads = downloadImage(destination, chemin, word_found_presentation, urls_images_presentation)
                     
-                    if 'success' in downloads:
-                        if (downloads['success']):
-                            line = destination + "/css/styles.css"
-                            search_css = ["--couleur-principale:", "--couleur-secondaire:", "--couleur-tertiaire:"]
-                            valeurs = [f"--couleur-principale: {colors_template[0]};", f"--couleur-secondaire: {colors_template[1]};", f"--couleur-tertiaire: {colors_template[2]};"]
-                            for (i, search) in enumerate(search_css) :
-                                css = writeFileCss(line, search_css[i], valeurs[i])
-                            
-                            # Je rappelle ma fonction pour zipper un dossier
-                            zipFolders = zipFolder(nameSite)
-    
-                            if 'success' in zipFolders:
-                                if (zipFolders['success']):
+                    line = destination + "/css/style.css"
+                    search_css = ["--couleur-principale:", "--couleur-secondaire:", "--couleur-tertiaire:"]
+                    valeurs = [f"--couleur-principale: {colors_template[0]};", f"--couleur-secondaire: {colors_template[1]};", f"--couleur-tertiaire: {colors_template[2]};"]
+                    for (i, search) in enumerate(search_css) :
+                        css = writeFileCss(line, search_css[i], valeurs[i])
+                    
+                    # Je rappelle ma fonction pour zipper un dossier
+                    zipFolders = zipFolder(nameSite)
 
-                                    rmtreeFolders = rmtreeFolder(nameSite)
-                                    if 'success' in rmtreeFolders:
-                                        if (rmtreeFolders['success']):
+                    if 'success' in zipFolders:
+                        if (zipFolders['success']):
 
-                                            return {"success": True, "message:": "Site généré avec succès !", "link_generate": "http://localhost:8000/download_zip/" + rmtreeFolders["nom_fichier"]}
-                                    # return {"success": True, "message": "Site généré !"}
+                            rmtreeFolders = rmtreeFolder(nameSite)
+                            if 'success' in rmtreeFolders:
+                                if (rmtreeFolders['success']):
+
+                                    return {"success": True, "message:": "Site généré avec succès !", "link_generate": "http://localhost:8000/download_zip/" + rmtreeFolders["nom_fichier"]}
+                            # return {"success": True, "message": "Site généré !"}
     else :
         return {"error": "Une erreur est survenue lors de la génération !"}
 
@@ -317,8 +378,7 @@ def writeArticleOfSite(chemin, occurence, articles):
                 lines_find = [i for i, line in enumerate(lines) if occurence in line]
 
             if (len(all_occurences) > 0) :
-                if(len(all_occurences) == len(articles)):
-                    [lines.__setitem__(x, re.sub(all_occurences[i], articles[i], lines[x], flags=re.MULTILINE) + '\n') for i, x in enumerate(lines_find) if 0 <= x < len(lines)]
+                [lines.__setitem__(x, re.sub(all_occurences[i], articles[i], lines[x], flags=re.MULTILINE) + '\n') for i, x in enumerate(lines_find) if 0 <= x < len(lines)]
                 
                 # Écrire la liste modifiée dans le fichier
                 with open(chemin, 'w', encoding='utf-8') as file:
